900197fce7987a96486e866e58e0cc45867aa3ea
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-22 18:12:30 +0100

    split exercises in two separate folders

--- dice_game/assets/js/main.js
+++ dice_game/assets/js/main.js
@@ -1,24 +1,3 @@
-// MAIL EXERCISE
-
-const inputEmail = prompt('Inerire la mail da controllare');
-
-const ListEmail = ['terry@gmail.com', 'jake@gmail.com', 'ginalinetti@gmail.com', 'charlesboyle@gmail.com', 'hitchcock@hotmail.com'];
-const containerElement= document.querySelector('.container')
-let outputMessage = document.createElement('h1');
-outputMessage.classList.add('alert')
-let check = false;
-for (let i = 0; i < ListEmail.length; i++) {
-    const element = ListEmail[i];
-    if (inputEmail === element) {
-    check = true;
-}
-}
-if (check === true) {
-    outputMessage.append('SEI PRESENTE NELLA MAIL LIST!');
-} else{
-    outputMessage.append('MI DISPIACE! MA NON SEI PRESENTE NELLA MAIL LIST');
-}
-containerElement.append(outputMessage);
 
 // DICE GAME EXERCISE
 
