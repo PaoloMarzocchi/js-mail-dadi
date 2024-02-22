7ec962d4372f5bbc27eca49fad0150ee608c64eb
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-22 17:36:46 +0100

    add dice game structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
+// MAIL EXERCISE
+
 const inputEmail = prompt('Inerire la mail da controllare');
 
 const ListEmail = ['terry@gmail.com', 'jake@gmail.com', 'ginalinetti@gmail.com', 'charlesboyle@gmail.com', 'hitchcock@hotmail.com'];
 const containerElement= document.querySelector('.container')
 let outputMessage = document.createElement('h1');
+outputMessage.classList.add('alert')
 let check = false;
 for (let i = 0; i < ListEmail.length; i++) {
     const element = ListEmail[i];
@@ -16,3 +19,39 @@ if (check === true) {
     outputMessage.append('MI DISPIACE! MA NON SEI PRESENTE NELLA MAIL LIST');
 }
 containerElement.append(outputMessage);
+
+// DICE GAME EXERCISE
+
+const inputDice = document.querySelector('.btn');
+
+inputDice.addEventListener('click', function(){
+
+    let diceResults = [];
+    let textResult = document.querySelector('h2');
+    let userResult = document.querySelector('.user');
+    let cpuResult = document.querySelector('.cpu');
+
+    for(i=0; i<2 ; i++){
+        function userGenerate(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+          }
+          diceResults.push(userGenerate(1,7));       
+    }
+    //console.log(diceResults);
+    userResult.innerHTML = diceResults[0];
+    cpuResult.innerHTML = diceResults[1];
+
+    if (diceResults[0] > diceResults[1]) {
+        //console.log('HAI VINTO');
+        textResult.innerHTML = 'HAI VINTO!'
+    } else if (diceResults[0] < diceResults[1]) {
+        //console.log('HAI PERSO');
+        textResult.innerHTML = 'HAI PERSO!'
+    } else{
+        //console.log('PAREGGIO');
+        textResult.innerHTML = 'PAREGGIO!'
+    }
+})
+
+
+//document.getElementById('demo').innerHTML = userGenerate(0,10)
